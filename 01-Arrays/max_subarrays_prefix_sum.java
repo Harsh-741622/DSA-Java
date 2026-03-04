3da1d9cc8d166169b2e342ced4d42313c89b10aa
Harsh-741622 <harshsharma.7416.in@gmail.com>
2026-03-04 20:12:51 +0530

    Add maximum subarray sum calculation
    Implement maximum subarray sum using prefix sum approach.

---
+++ 01-Arrays/max_subarrays_prefix_sum.java
@@ -0,0 +1,42 @@
+// Problem: Maximum Subarrays Sum 
+// Approach: Prefix Sum
+// Time Complexity: O(n^2) 
+// Space Complexity: -
+// Source: Apna College Sigma Batch
+
+public class Main {
+    public static void main(String args[]) {
+        int[] numbers = { 2, 4, 1, 5, -2, 5 };
+        MaxSumOfSubarrays(numbers);
+
+    }
+
+    public static void MaxSumOfSubarrays(int[] numbers) {
+        int n = numbers.length;
+        int sum = 0;
+        // Creating Prefix sum array : 
+        int[] prefixSumArray = new int[n];
+        for (int i = 0; i < numbers.length; i++) {
+            sum = sum + numbers[i];
+            prefixSumArray[i] = sum;
+
+        }
+        int maxSum = Integer.MIN_VALUE;
+        // Finding max :
+        for (int i = 0; i < n; i++) {
+            int start = i ; 
+            for (int j = i; j < n; j++) {
+                int end = j;
+                int currSum = 0 ; 
+                // currSum = prefixSumarray[end] - prefixSumarray[start -1];  
+                currSum = start == 0 ? prefixSumArray[end] : prefixSumArray[end] - prefixSumArray[start-1] ;
+                if(maxSum < currSum){
+                    maxSum = currSum; 
+                }
+               
+            }
+        }
+        System.out.println("Max sum " + maxSum);
+
+    }
+}
