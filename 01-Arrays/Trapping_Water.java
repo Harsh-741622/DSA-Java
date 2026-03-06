8693c790258d11b4afd10ede22499f925078a0d1
Harsh-741622 <harshsharma.7416.in@gmail.com>
2026-03-06 15:38:03 +0530

    Create Trapping_Water.java

---
+++ 01-Arrays/Trapping_Water.java
@@ -0,0 +1,40 @@
+
+import java.util.*;
+
+public class Main {
+     public static int TrappingRainwater(int[] height){
+        int n = height.length;
+        // Max Left boundary 
+        int[] MaxLeftB = new int[n];
+        MaxLeftB[0] = height[0];
+        for(int i=1 ; i<n ; i++){
+            MaxLeftB[i] = Math.max(height[i],MaxLeftB[i-1]);
+        }
+        // Max Right boundary 
+        int[] MaxRightB = new int[n];
+        MaxRightB[n-1] = height[n-1];
+        for(int i=n-2 ; i>=0 ; i--){
+            MaxRightB[i] = Math.max(height[i],MaxRightB[i+1]);
+        }
+        // loop
+        int trappingwater = 0 ;
+        for(int i=0 ; i<n ; i++){
+            // Waterlevel
+            int waterlevel = Math.min(MaxLeftB[i],MaxRightB[i]);
+            // Trapping water 
+            trappingwater = trappingwater + (waterlevel - height[i]);
+        }
+        return trappingwater; 
+          
+         
+     }
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        // int[] height = {1,4,5,6,3,2,6,7,8,4,5,9,7,4};
+        int[] height = {4,2,0,6,3,2,5};
+        int toatalwater = TrappingRainwater(height);
+        System.out.println(toatalwater);
+       
+    }
+}
